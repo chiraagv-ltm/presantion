4477f5243e9400b9796942de0eeb691ce4a4b2d3
chiraagv-ltm <82768667+chiraagv-ltm@users.noreply.github.com>
2021-11-10 09:35:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function one(){
+    window.alert("El mapa de Venezuela. Venezuela es a español discurso americano del sur país de a federal presidencial república aquel consistir de veintitrés estado"); 
+  }
+  function two(){
+    window.alert("Venezula tiene desierto, selva montanas y tropical jungla"); 
+  }
+  function three1(){
+    window.alert(" Venezuela tiene troupial venezolano, es el ave nacional de Venezuela  "); 
+  }
+  function three2(){
+    window.alert("Venezuela tiene Cattleya mossiae, es la flor nacional de venezuela "); 
+  }
+  function three3(){
+    window.alert("Venezuela tiene monoes aulladores, se encuentran principalmente en Venezuela"); 
+  }
+  function three4(){
+    window.alert("Venuzuala tiene capibara , se encuentran principalmente en Venezuela"); 
+  }
+  function three5(){
+    window.alert("Venuzuala tiene oso hormiguero gigante , se encuentran principalmente en Venezuela"); 
+  }
+  function three6(){
+    window.alert("Venuzuala tiene nutria gigante , se encuentran principalmente en Venezuela"); 
+  }
+  function three7(){
+    window.alert("Venuzuala tiene mono araña de vientre blanco , se encuentran principalmente en Venezuela"); 
+  }
+  function three8(){
+    window.alert("Venuzuala tiene , se encuentran principalmente en Venezuela"); 
+  }
+  function four(){
+    window.alert("Famous for its rock-cut architecture and water conduit system, Petra is also called the Rose City because of the colour of the stone from which it is carved. It has been a UNESCO World Heritage Site since 1985.Petra is a symbol of Jordan, as well as Jordan's most-visited tourist attraction"); 
+  }
+  function five(){
+    window.alert("Chichen Itza was a large pre-Columbian city built by the Maya people of the Terminal Classic period. The archaeological site is located in Tinúm Municipality"); 
+  }
+  function six(){
+    window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe"); 
+  }
+  function seven(){
+    window.alert("Christ The Redeemer is an Art Deco statue of Jesus Christ in Rio de Janeiro, Brazil, created by Polish-French sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot. Romanian sculptor Gheorghe Leonida fashioned the face. Constructed between 1922 and 1931, the statue is 30 metres (98 ft) high, excluding its 8-metre (26 ft) pedestal. The arms stretch 28 metres (92 ft) wide.[1][2]"); 
+  }
